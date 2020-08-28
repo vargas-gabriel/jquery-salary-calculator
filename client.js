98f39d98514e70ea763a928adc3a0185651dcae4
Gabriel Vargas <vargasgabriel@protonmail.com>
2020-08-28 17:48:34 -0500

    button input pushes info to DOM

--- client.js
+++ client.js
@@ -18,12 +18,21 @@ function addEmployee(){
     employeeInfo.push( newItem );
     console.log( employeeInfo );
     calculateSalary();
+    displayEmployeeInfo();
 }//end addEmployee
 
 function calculateSalary(){
-    
+    console.log( 'in calculateSalary');
+
 }
 
 function displayEmployeeInfo(){
     console.log( 'in displayEmployeeInfo' );
+    let el = $( '#employeeInfoOut' );
+    el.empty();
+    for (let i = 0; i < employeeInfo.length; i++ ) {
+        el.append(`<li> FirstName: ${ employeeInfo[i].FirstName}  LastName: ${ employeeInfo[i].LastName }  ID: 
+    ${ employeeInfo[i].ID }  Title: ${ employeeInfo[i].Title}  AnnualSalary: ${ employeeInfo[i].AnnualSalary} </li>` );
+    }
+    
 }
