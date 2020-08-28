09d8c175f0f344ffb6451264baa1122ba2ee092f
Gabriel Vargas <vargasgabriel@protonmail.com>
2020-08-28 17:16:31 -0500

    capture click event, push input info into array

--- client.js
+++ client.js
@@ -1,8 +1,19 @@
 console.log( 'js' );
 $(document).ready(onReady);
+let employeeInfo = [];
 function onReady() {
     $('#submitButton').on('click', addEmployee);
-}
+}//end onReady
 function addEmployee(){
     console.log( 'you clicked me' );
-}
+    const newItem = {
+        FirstName: $('#firstNameIn').val(),
+        LastName: $('#lastNameIn').val(),
+        ID: $('#idIn').val(),
+        Title: $('#titleIn').val(),
+        AnnualSalary: $('#annualSalaryIn').val()
+    }//end new item
+    console.log( 'newItem:', newItem );
+    employeeInfo.push( newItem );
+    console.log( employeeInfo );
+}//end addEmployee
